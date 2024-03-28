a315b34872b846ba62be34eb1d210fad04208589
adityajunwal <163153177+adityajunwal@users.noreply.github.com>
2024-03-29 03:03:11 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+/*document.getElementById("submit-button").addEventListener("click", function() {
+    var map = document.createElement("iframe");
+    map.src = "https://www.google.com/maps/d/embed?mid=1cheNSd0uLbYs33nQsJL4j1YRhMsvwpI&ehbc=2E312F&noprof=1";
+    map.width = 480;
+    map.height = 350;
+    this.disabled = true;
+    document.getElementById("input-container").appendChild(map);
+})*/
