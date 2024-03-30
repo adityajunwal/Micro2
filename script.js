00c663f2f6d97bb6481bf7a170b10f0522564881
adityajunwal <163153177+adityajunwal@users.noreply.github.com>
2024-03-30 22:54:55 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
-/*document.getElementById("submit-button").addEventListener("click", function() {
-    var map = document.createElement("iframe");
-    map.src = "https://www.google.com/maps/d/embed?mid=1cheNSd0uLbYs33nQsJL4j1YRhMsvwpI&ehbc=2E312F&noprof=1";
-    map.width = 480;
-    map.height = 350;
-    this.disabled = true;
-    document.getElementById("input-container").appendChild(map);
-})*/
+var busNumber = localStorage.getItem('busNumber');
+
+fetch(`${busNumber}.json`)
+  .then(response => response.json())
+  .then(data => {
+    // Use the parsed JSON data here
+    console.log(data);
+  })
+  .catch(error => console.error('Error fetching JSON:', error));
