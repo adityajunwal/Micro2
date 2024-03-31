9acc1a1d67746069446c062ad309626c4e5e9cce
adityajunwal <163153177+adityajunwal@users.noreply.github.com>
2024-03-31 12:55:37 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,9 +1,26 @@
 var busNumber = localStorage.getItem('busNumber');
 
-fetch(`${busNumber}.json`)
+fetch(`BusInfo/${busNumber}.json`)
   .then(response => response.json())
   .then(data => {
-    // Use the parsed JSON data here
-    console.log(data);
+    
+    document.getElementById("bus-number").innerText = `Bus Number: ${data["busNumber"]}`;
+    document.getElementById("driver-name").innerText = `Driver: ${data["driverName"]}`;
+    document.getElementById("bus-reg").innerText = `Bus Registration: ${data['registrationNumber']}`;
+
+    var table = "";
+    data["routeTable"].forEach(element => {
+      var row = `<tr>
+                <td>${element["serialNumber"]}</td>
+                <td>${element["busStop"]}</td>
+                <td>${element["timing1"]}</td>
+                <td>${element["timing2"]}</td>
+                </tr>`
+      table += row;
+    });
+
+    document.getElementById("table-body").innerHTML = table;
+    console.log(data["busNumber"]);
   })
   .catch(error => console.error('Error fetching JSON:', error));
+
